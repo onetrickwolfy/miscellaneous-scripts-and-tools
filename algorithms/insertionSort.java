76195201f10303a7bad91fbbba3c4f03679268e0
Gabey <123382040+onetrickwolfy@users.noreply.github.com>
2023-01-31 14:14:06 +0100

    Update insertionSort.java

--- algorithms/insertionSort.java
+++ algorithms/insertionSort.java
@@ -15,8 +15,6 @@ public class insertionSort {
                 array[j] = prev;
                 j--;
             }
-
-            myArrayUtils.print_array(array);
         }
 
         return array;
